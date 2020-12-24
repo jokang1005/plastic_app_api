b0f674642f87d1b363491afe274888c07e3f852f
Joanne Lee <jokang1005@gmail.com>
2020-12-23 19:57:04 -0600

    had to rake db:drop db:create db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_005421) do
+ActiveRecord::Schema.define(version: 2020_12_24_012215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,10 @@ ActiveRecord::Schema.define(version: 2020_12_24_005421) do
     t.integer "plastic_cup"
     t.integer "disposable_diaper"
     t.integer "plastic_toothbrush"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_plastics_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_12_24_005421) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "plastics", "users"
 end
