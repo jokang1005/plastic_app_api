7a536eff7ed30a300017fbb7c67e9db2ead26d8f
Joanne Lee <jokang1005@gmail.com>
2020-12-23 19:07:52 -0600

    modified application_controller.rb for JWT

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_184522) do
+ActiveRecord::Schema.define(version: 2020_12_24_005421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2020_12_23_184522) do
     t.integer "plastic_cup"
     t.integer "disposable_diaper"
     t.integer "plastic_toothbrush"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
